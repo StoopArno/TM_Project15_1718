f3af04f3c513c348c4dd0f6d4029508f0c3e4964
Arno Stoop <r0657905@student.thomasmore.be>
2018-03-09 13:43:04 +0100

    Added content to the headers

--- application/views/views_administrator/admin_header.php
+++ application/views/views_administrator/admin_header.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sande
- * Date: 9/03/2018
- * Time: 11:18
- */
+<?php ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="/assets/images/logo_TM.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Dagonderdelen beheren</a>
+            <a class="nav-item nav-link" href="#">Fuif beheren</a>
+            <a class="nav-item nav-link" href="#">Eten beheren</a>
+            <a class="nav-item nav-link" href="#">Overzicht deelnemers/helpers</a>
+            <a class="nav-item nav-link" href="#">Mails</a>
+            <a class="nav-item nav-link" href="#">Vervoer beheren</a>
+            <a class="nav-item nav-link" href="#">Vervoer beheren</a>
+        </div>
+    </div>
+</nav>
