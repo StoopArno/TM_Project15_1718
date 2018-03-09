251e576a6919c5741070940bf6737c4a20a16800
Arno Stoop <r0657905@student.thomasmore.be>
2018-03-09 15:12:07 +0100

    Modified the navbar logo

--- application/views/views_administrator/admin_header.php
+++ application/views/views_administrator/admin_header.php
@@ -1,7 +1,8 @@
 <?php ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-        <img src="/assets/images/logo_TM.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+        <?php echo toonAfbeelding("logo_TM.png", array( "class" => "d-inline-block align-top", "height" => "50px"))?>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
