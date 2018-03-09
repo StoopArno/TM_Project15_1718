75fd82d019ec8746fff2c0d669b7e27c004f1df4
Dean <deanclerckx@hotmail.com>
2018-03-09 14:03:53 +0100

    update main_footer

--- application/views/main_footer.php
+++ application/views/main_footer.php
@@ -1,1 +1,62 @@
-<?php
+    <!-- Footer -->
+<footer class="page-footer font-small blue pt-4 mt-4">
+
+    <!--Footer Links-->
+    <div class="container-fluid text-center text-md-left">
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-4">
+                <h5 class="text-uppercase">Footer Content</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <p>Dean Clerckx</p>
+                    </li>
+                    <li>
+                        <p>Yorben Onsia</p>
+                    </li>
+                    <li>
+                        <p>Arno stoop</p>
+                    </li>
+                    <li>
+                        <p>Lindert Van De Poel</p>
+                    </li>
+					<li>
+                        <p>Sander Philipsen</p>
+                    </li>
+                </ul>
+            </div>
+            <!--/.First column-->
+
+            <!--Second column-->
+            <div class="col-md-4">
+                <h5 class="text-uppercase">Eindverantwoordelijke:</h5>
+				<p>variabele</p>
+            </div>
+            <!--/.Second column-->
+			
+			<!--third column-->
+            <div class="col-md-4">
+                <h5 class="text-uppercase">Opdrachtgever:</h5>
+				<p> Natalie Smets</p>
+            </div>
+            <!--/.third column-->
+        </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3 text-center">
+        © 2018 Copyright:
+        <p>Team 15 : Whiskeypedia</p>
+    </div>
+    <!--/.Copyright-->
+</footer>
+
+
+</div>
+
+
+</body>
+
+</html>
