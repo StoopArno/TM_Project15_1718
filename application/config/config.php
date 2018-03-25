bc0caf792eac67eae6867c862b29fb8d1593d1b5
Lindert Vdp <Lindert_vdp@hotmail.com>
2018-03-25 18:02:24 +0200

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/projectapp/project15_1718/';
+$config['base_url'] = 'http://localhost:81/project/project15_1718/';
 
 /*
 |--------------------------------------------------------------------------
