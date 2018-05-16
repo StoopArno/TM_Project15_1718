ddd80a415167be848e6465415bdb5c2e1ee52246
Lindertvdp <Lindert_vdp@hotmail.com>
2018-05-16 14:13:14 +0200

    Doxygen

--- application/views/views_helper/helper_fotos_raadplegen.php
+++ application/views/views_helper/helper_fotos_raadplegen.php
@@ -2,7 +2,6 @@
 /**
  * @file views_helper/helper_fotos_raadplegen.php
  *
-<<<<<<< HEAD
  * Hier kan de helper leuke herinneringen ophalen en foto's van afgelopen jaren bekijken.
  */
 ?>
@@ -12,12 +11,6 @@
 
 </script>
 
-
-=======
- * View die een overzicht weergeeft van de fotos van de voorbije jaren.
- */
-?>
->>>>>>> 90e8f67fdfe1e9f7ae7a66c3723817734353784a
 <article>
     <div class="btn-group">
         <?php
